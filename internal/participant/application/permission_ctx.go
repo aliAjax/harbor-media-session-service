c1d9eefb57f324dd692075819f262a398e5af2d7
aliAjax <18640215790@163.com>
2026-08-23 19:32:34 +0800

    feat: initial import

---
+++ internal/participant/application/permission_ctx.go
@@ -0,0 +1,5 @@
+package application
+
+import "context"
+
+func guardPermissionContext(ctx context.Context) error { return nil }
---
+++ internal/participant/application/session_get_ctx.go
@@ -0,0 +1,5 @@
+package application
+
+import "context"
+
+func guardSessionGetContext(ctx context.Context) error { return nil }
